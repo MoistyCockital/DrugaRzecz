611cb82ba7a6a39b790f0d045f425becef865de0
MoistCockital <rugygejonator.yes@gmail.com>
2022-11-24 10:44:11 +0100

    Nwe okno

---
+++ DrugaRzecz/ObliczeniaWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DrugaRzecz
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ObliczeniaWindow.xaml
+    /// </summary>
+    public partial class ObliczeniaWindow : Window
+    {
+        public ObliczeniaWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
